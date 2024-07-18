7e54a175edf8fbdc701315970204977925c1ad96
Xandr Stagwood <132042461+xandrsgit@users.noreply.github.com>
2024-07-18 16:09:03 +0000

    added the ability to function without a database and to set status letting the owner know that database is down. Also added setup instructions.

--- voices.py
+++ voices.py
@@ -1,11 +1,9 @@
 import os
 import discord
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 import asyncio
 import re
-import logging
-from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
 # Load environment variables from .env file
@@ -13,30 +11,16 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 MONGO_URI = os.getenv('MONGO_URI')
 
-# Connect to MongoDB
-client = MongoClient(MONGO_URI)
-db = client['your_database_name']
-logs_collection = db['logs']
+# Connect to MongoDB with error handling
+try:
+    client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+    db = client['voices']
+    logs_collection = db['logs']
+    client.server_info()  # Force connection on a request as the connect=True parameter of MongoClient seems to be useless here
+except errors.ServerSelectionTimeoutError as err:
+    print(f"MongoDB connection error: {err}")
+    exit(1)
 
-# Configure logging
-def setup_logger(guild_id):
-    now = datetime.now()
-    log_directory = f"logs/{guild_id}/{now.year}/{now.month}"
-    os.makedirs(log_directory, exist_ok=True)
-    log_path = f"{log_directory}/{now.day}.log"
-
-    logger = logging.getLogger(f'guild_{guild_id}')
-    logger.setLevel(logging.INFO)
-
-    if not logger.handlers:
-        handler = RotatingFileHandler(log_path, maxBytes=5*1024*1024, backupCount=3)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-    return logger
-
-# Setup Discord client
 intents = discord.Intents.default()
 intents.guilds = True
 intents.members = True
@@ -48,12 +32,31 @@ queue = asyncio.Queue()
 def sanitize_nickname(nickname):
     return re.sub(r'[^a-zA-Z0-9-_]', '', nickname)[:32]
 
+async def log_event(event_type, guild, **kwargs):
+    log_entry = {
+        "event": event_type,
+        "guild": guild.name,
+        "guild_id": guild.id,
+        "timestamp": datetime.now(),
+    }
+    log_entry.update(kwargs)
+    logs_collection.insert_one(log_entry)
+    # Uncomment the following line to enable printing to the terminal
+    # print(f"Logged event: {log_entry}")
+
+async def update_status():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        server_count = len(bot.guilds)
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"over {server_count} servers"))
+        await asyncio.sleep(600)  # Update every 10 minutes
+
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
         await queue.put(ensure_voices_category_and_channel(guild))
     bot.loop.create_task(process_queue())
-    print(f'Logged in as {bot.user}')
+    bot.loop.create_task(update_status())
 
 @bot.event
 async def on_guild_join(guild):
@@ -66,23 +69,21 @@ async def process_queue():
         await asyncio.sleep(1)
 
 async def ensure_voices_category_and_channel(guild):
-    logger = setup_logger(guild.id)
     try:
         bot_member = guild.get_member(bot.user.id)
         if bot_member and bot_member.guild_permissions.manage_channels:
             voices_category = discord.utils.get(guild.categories, name="VOICES")
             if not voices_category:
                 voices_category = await guild.create_category("VOICES")
-                logger.info(f"'VOICES' category created in guild {guild.name} ({guild.id})")
+                await log_event("category_created", guild, category="VOICES")
             if not discord.utils.get(voices_category.channels, name="➕︱voice"):
                 await voices_category.create_voice_channel("➕︱voice")
-                logger.info(f"'➕︱voice' channel created in guild {guild.name} ({guild.id})")
+                await log_event("channel_created", guild, channel="➕︱voice")
     except (discord.Forbidden, discord.HTTPException) as e:
-        logger.error(f"Error in guild {guild.name} ({guild.id}): {e}")
+        await log_event("error", guild, error=str(e))
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    logger = setup_logger(member.guild.id)
     if after.channel and after.channel.name == "➕︱voice":
         voices_category = discord.utils.get(member.guild.categories, name="VOICES")
         if voices_category:
@@ -90,6 +91,7 @@ async def on_voice_state_update(member, before, after):
             user_channel = discord.utils.get(voices_category.channels, name=sanitized_nickname)
             if user_channel:
                 await member.move_to(user_channel)
+                await log_event("user_moved", member.guild, member=member.nick or member.name, channel=user_channel.name)
             else:
                 await queue.put(create_and_move_to_channel(member, voices_category, sanitized_nickname))
     if before.channel and before.channel != after.channel:
@@ -98,26 +100,22 @@ async def on_voice_state_update(member, before, after):
                 await queue.put(delete_channel(before.channel))
 
 async def create_and_move_to_channel(member, category, channel_name):
-    logger = setup_logger(member.guild.id)
     try:
         new_channel = await category.create_voice_channel(channel_name)
         await member.move_to(new_channel)
-        logger.info(f"Created and moved {member.nick or member.name} to channel {channel_name} in {category.guild.name}")
-        logs_collection.insert_one({"event": "channel_created", "member": member.nick or member.name, "channel": channel_name, "timestamp": datetime.now()})
+        await log_event("channel_created", category.guild, member=member.nick or member.name, channel=channel_name)
     except (discord.Forbidden, discord.HTTPException) as e:
-        logger.error(f"Error for {member.nick or member.name}: {e}")
+        await log_event("error", category.guild, error=str(e))
         if isinstance(e, discord.HTTPException) and e.status == 429:
             await asyncio.sleep(int(e.response.headers['Retry-After']) / 1000)
             await create_and_move_to_channel(member, category, channel_name)
 
 async def delete_channel(channel):
-    logger = setup_logger(channel.guild.id)
     try:
         await channel.delete()
-        logger.info(f"Deleted empty channel {channel.name} in {channel.guild.name}")
-        logs_collection.insert_one({"event": "channel_deleted", "channel": channel.name, "timestamp": datetime.now()})
+        await log_event("channel_deleted", channel.guild, channel=channel.name)
     except (discord.Forbidden, discord.HTTPException) as e:
-        logger.error(f"Error deleting {channel.name}: {e}")
+        await log_event("error", channel.guild, error=str(e))
         if isinstance(e, discord.HTTPException) and e.status == 429:
             await asyncio.sleep(int(e.response.headers['Retry-After']) / 1000)
             await delete_channel(channel)
